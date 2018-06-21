cf4bc73b7ce15fe65e1d087467a8eaa0feed6348
Andrey Kononykhin <andkononykhin@gmail.com>
2018-06-21 16:13:08 +0300

    adds test that check dates extraction as YYYY-MM-DD

--- library_test.py
+++ library_test.py
@@ -30,6 +30,10 @@ class TestCase(unittest.TestCase):
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    def test_dates(self):
+        self.assert_extract('I was born on 2015-07-25.',
+                            library.dates_iso8601, '2015-07-25')
+
 
 if __name__ == '__main__':
     unittest.main()
